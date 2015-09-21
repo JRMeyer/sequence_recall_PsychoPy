4a878579b1e2c4bff1453adecd7004ea8c65e58d
JRMeyer <joshua.richard.meyer@gmail.com>
2015-09-21 15:19:57 -0700

    moved config.txt to inside SeqRec_master

--- SeqRec.py
+++ SeqRec.py
@@ -2,7 +2,7 @@
 '''
 
 USAGE: 
-$ python sequence_recall.py
+$ python SeqRec.py
 
 DEPENDENCIES:
 template.py
@@ -53,7 +53,7 @@ import ast
 
 
 
-class Seqrec():
+class SeqRec():
     def __init__(self):
         self.config()
         self.templateList = templateList
@@ -62,41 +62,115 @@ class Seqrec():
 
 
     def config(self):
-        if os.path.isfile('config.txt'): 
-            with open('config.txt') as f:
-                lines = f.readlines()
+        if os.path.isfile('SeqRec_master/config.txt'):
+            try:
+                with open('SeqRec_master/config.txt') as f:
+                    lines = f.readlines()
 
-            for line in lines:
-                label,values = line.strip().split('\t')
-                if label == 'contrasts':
-                    self.contrasts = ast.literal_eval(values)
-                elif label == 'speakers':
-                    self.speakers = ast.literal_eval(values)
+                for line in lines:
+                    label,values = line.strip().split('\t')
+                    print label
+                    print values
+                    if label == 'contrasts':
+                        self.contrasts = ast.literal_eval(values)
+                    elif label == 'speakers':
+                        self.speakers = ast.literal_eval(values)
+                    elif label == 'testCutOff':
+                        self.testCutOff = float(ast.literal_eval(values)[0])
+                    elif label == 'testISI':
+                        self.testISI = float(ast.literal_eval(values)[0])
+                    elif label == 'mainISI':
+                        self.mainISI = float(ast.literal_eval(values)[0])
+            except:
+                print "ERROR: The config.txt file is not correctly formatted."
         else:
+            print '\n'
+            
+            print("Welcome to SeqRec! It seems you are a new user. We need\n"+
+            "to complete a few steps in order to set up your experiment. You\n"+
+            "will now be asked a series of questions, and that information\n"+
+            "will be used to configure the experiment.\n"+
+            "If you do this accurately (and do not delete or move the\n"+
+            "config.txt file which will be produced), you should only need\n"+
+            "to complete this process once.")
+
+            print '\n'
+            raw_input("Press any key to continue")
+            print '\n'
+            
+            print("In the following questions, you will be asked for numbers\n"+
+            "and labels of stimuli. You should enter the labels as they\n"+
+            "appear in your filenames for the auditory stimuli. Any typos\n"+
+            "will result in failure of the program to find your files. So be\n"+
+            "careful to enter them correctly.")
+
+            print '\n'
+            raw_input("Press any key to continue")
+            print '\n'
+            
+            print "Let's get started!"
+            print '\n'
+            core.wait(1)
+            
             speakers=[]
-            numSpeakers = raw_input("How many speakers do you have for each stimulus? Enter a number here: ")
+            numSpeakers = raw_input("How many speakers are there for each"+
+            " word?\n\tEnter a number here: ")
+            print '\n'
+            
             for i in range(1,int(numSpeakers)+1):
-                speakers.append(raw_input("Please type in speaker " +str(i)+
+                speakers.append(raw_input("Enter speaker " +str(i)+
                             " here, as it appears in your audio filenames :  "))
                 print '\n'
 
             contrasts=[]
-            numContrasts = raw_input("How many sound contrasts do you have? Enter a number here: ")
+            numContrasts = raw_input("How many sound contrasts do you have?\n"+
+            "\tEnter a number here: ")
+            print '\n'
+            
             for i in range(1,int(numContrasts)+1):
                 contrast=[]
-                contrast.append(raw_input("Please type in first word for contrast " +str(i)+
-                            " here, as it appears in your audio filenames :  "))
-                contrast.append(raw_input("Please type in second word for contrast " +str(i)+
-                            " here, as it appears in your audio filenames :  "))
-                contrasts.append(contrast)
+                contrast.append(raw_input("Enter first word for contrast "
+                                          +str(i)+ " here, as it appears in "+
+                                           "the audio filenames :  "))
                 print '\n'
-            self.speakers = speakers
-            self.contrasts = contrasts
+                contrast.append(raw_input("Enter second word for "+
+                                          "contrast " +str(i)+ " here, as it "+
+                                           "appears in the audio filenames : "))
+                print '\n'
 
-            configFile = open('config.txt', 'w')
+                contrasts.append(contrast)
+
+
+            testCutOff = [raw_input("How many correct responses in a row does\n"+
+                            "the participant need to pass the testing phase?\n"+
+                                   "\tEnter a number here: ")]
+            print '\n'
+
+            
+            testISI = [raw_input("What inter-stimulus interval (ISI) would\n"+
+                            "you like for the testing phase?\n"+
+                                   "\tEnter a number here, in seconds: ")]
+            print '\n'
+
+            
+            mainISI = [raw_input("What inter-stimulus interval (ISI) would\n"+
+                            "you like between sounds within sequences during\n"+
+                            "the main phase of the experiment?\n"+
+                                "\tEnter a number here, in seconds: ")]
+            print '\n'
+            
+            configFile = open('SeqRec_master/config.txt', 'w')
             print >> configFile, 'speakers\t' + str(speakers)
             print >> configFile, 'contrasts\t' + str(contrasts)
-                
+            print >> configFile, 'testCutOff\t' + str(testCutOff)
+            print >> configFile, 'testISI\t' + str(testISI)
+            print >> configFile, 'mainISI\t' + str(mainISI)
+            
+            print "Configuration complete!\n"
+            print "You may restart the experiment now.\n"
+            print "Enjoy!\n"
+
+            sys.exit()
                     
 
         
@@ -171,31 +245,31 @@ class Seqrec():
         self.win.flip()                                                         # flip to the buffer, which is blank, so we don't have
                                                                                 # the text sitting on the screen indefinitely
     
-    def play_list_WAVs(self, _list, isi):                                       # Loops through a list of WAVs and plays them with 
+    def play_list_WAVs(self, _list, isi):                                            # Loops through a list of WAVs and plays them with 
         try:                                                                                # a given inter-stimulus interval (isi)
             for WAV in _list:
-                my_sound = sound.Sound(value=WAV, sampleRate=44100, bits=16, 
+                mySound = sound.Sound(value=WAV, sampleRate=44100, bits=16, 
                                        name='', autoLog=True)                       # create sound object with all info needed
-                my_sound.play()                                                     # actually play the sound
-                core.wait(my_sound.getDuration()+isi)                               # You need the duration of the sound here, 
+                mySound.play()                                                     # actually play the sound
+                core.wait(mySound.getDuration()+isi)                               # You need the duration of the sound here, 
                                                                                     # or else we play overlappying sounds
         except:
             print "ERROR: audio files not found"
             self.win.close()
             sys.exit()
     
-    def play_one_level(self, level, isi=.08):                                   # Play one level of sequences
+    def play_one_level(self, level):                                            # Play one level of sequences
         random.shuffle(level)                                                   # do the shuffle
         self.levelList=[]                                                       # create an empty list
         for seq in level:                                                       # pull out one sequence
-            self.play_list_WAVs(seq[1:],isi)                                    # the the list, but starting from the second element,
+            self.play_list_WAVs(seq[1:],self.mainISI)                                    # the the list, but starting from the second element,
                                                                                 # the first element is just a character string of 
                                                                                 # type "A_A_B_A"
 
             beep = sound.Sound(value="E", secs=.3, bits=16, name='',            # create sound object (just a beep to signal end
                                autoLog=True)                                    # of sequence)
             beep.play()                                                         # actually play the beep
-            core.wait(beep.getDuration()+isi)                                   # wait to collect responses
+            core.wait(beep.getDuration()+self.mainISI)                                   # wait to collect responses
             self.collect_responses(seq,waitingTime=5)                           # gather the responses
             core.wait(.5)                                                       # wait a half of a second before next level
     
@@ -290,36 +364,38 @@ class Seqrec():
         self.win.flip()
         
     
-    def testing_phase(self, isi=.6, testCutoff=7, i=0):
+    def testing_phase(self):
         '''
         Participant must correctly identify a given number of stimuli
         in a row to proceed. A circle is desplayed to participant to 
         indicate progress.
         '''
-        outerCircle = visual.Circle(self.win, radius = 280, edges = 64,
+        circleRadius=280
+        outerCircle = visual.Circle(self.win, radius = circleRadius, edges = 64,
                                     lineColor="green", fillColor="green")
-        innerCircle = visual.Circle(self.win, radius = 280, edges = 64,
+        innerCircle = visual.Circle(self.win, radius = circleRadius, edges = 64,
                                     lineColor="green", fillColor="green", 
                                     contrast=.15)
-        while i < testCutoff:
+        i = 0
+        while i < self.testCutOff:
             if i == 0:
-                visual.Circle(self.win, radius = 280, edges = 64, 
+                visual.Circle(self.win, radius = circleRadius, edges = 64, 
                               lineColor="green", fillColor="green",
                               contrast=.15).draw()
                 self.win.flip()
             else:
                 outerCircle.draw()
-                innerCircle.setRadius(280-40*i)
+                innerCircle.setRadius(circleRadius-(circleRadius/self.testCutOff)*i)
                 innerCircle.draw()
                 self.win.flip()
             WAVlist = random.choice(range(len(self.AandB_Paths)))               # pick randomly the list of 'a' or 'b' tokens
             speaker= random.choice(range(len(self.AandB_Paths[1])))             # pick random speaker
             token= random.choice(range(len(self.AandB_Paths[1][1])))            # pick random token
             WAV = self.AandB_Paths[WAVlist][speaker][token]                     # assign audio file path to object WAV
-            my_sound= sound.Sound(value=WAV, sampleRate=44100, bits=16, name='',
+            mySound= sound.Sound(value=WAV, sampleRate=44100, bits=16, name='',
                                   autoLog=True)                                 # create the sound object with the audio stimulus WAV
-            my_sound.play()                                                     # play the sound object 
-            core.wait(my_sound.getDuration())                                   # wait until the sound is through playing
+            mySound.play()                                                     # play the sound object 
+            core.wait(mySound.getDuration())                                   # wait until the sound is through playing
             keyPress = event.waitKeys()                                         # wait for key press and save it to keyPress
             if keyPress == ["left"]:                                            # check if the left arrow was pressed
                 if bool(re.search("A.wav", WAV)):                               # check if the WAV path was an 'a' token
@@ -327,7 +403,7 @@ class Seqrec():
                                                                                 # gets credit for the answer
                     for frameN in range(15):
                         outerCircle.draw()
-                        innerCircle.setRadius(280-40*i)
+                        innerCircle.setRadius(circleRadius-(circleRadius/self.testCutOff)*i)
                         innerCircle.draw()
                         visual.TextStim(self.win, "CORRECT").draw()             # project "CORRECT" on the screen
                         self.win.flip()
@@ -340,7 +416,7 @@ class Seqrec():
                                                                                 # gets credit for the answer
                     for frameN in range(15):
                         outerCircle.draw()
-                        innerCircle.setRadius(280-40*i)
+                        innerCircle.setRadius(circleRadius-(circleRadius/self.testCutOff)*i)
                         innerCircle.draw()
                         visual.TextStim(self.win, "CORRECT").draw()             # project "CORRECT" on the screen
                         self.win.flip()
@@ -348,10 +424,10 @@ class Seqrec():
                     self.display_prompt("WRONG")
                     i=0
             outerCircle.draw()
-            innerCircle.setRadius(280-40*i)
+            innerCircle.setRadius(circleRadius-(circleRadius/self.testCutOff)*i)
             innerCircle.draw()
             self.win.flip()    
-            core.wait(isi)                                                      # wait before we play next WAV file
+            core.wait(self.testISI)                                          # wait before we play next WAV file
     
     def collect_responses(self, _list, waitingTime):                            # After the participant hears the sequence of WAVs, 
                                                                                 # we want to collect and save some key presses
@@ -448,5 +524,5 @@ class Seqrec():
 
 
 if __name__ == "__main__":
-    S = Seqrec()
+    S = SeqRec()
     S.run()
